37fba70ea7b954ce47c8c94b6658854fc18f72d1
koguchi-e <koguchi.e915@gmail.com>
2026-03-12 16:38:03 +0900

    姓名入力欄追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,28 +3,44 @@ import "./App.css";
 import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
-  const [input, setInput] = useState("");
+  const [firstName, setFirstName] = useState("");
+  const [lastName, setLastName] = useState("");
 
   return (
     <>
-      <div>
+      <div className="input-form mb-3">
         <h1 className="display-1">Call For Proposals</h1>
-        <div className="input-form mb-3">
-          <label htmlFor="name-input" className="form-label">
-            名前：
-          </label>
-          <input
-            id="name-input"
-            type="text"
-            value={input}
-            onChange={(e) => setInput(e.target.value)}
-            placeholder="名前を入力"
-            className="name-input form-control"
-          />
-          <button type="submit" className="add-button btn btn-primary">
-            登録
-          </button>
+        <div className="row">
+          <div className="col">
+            <label htmlFor="name-input" className="form-label">
+              姓：
+            </label>
+            <input
+              id="name-input"
+              type="text"
+              value={firstName}
+              onChange={(e) => setFirstName(e.target.value)}
+              placeholder="姓を入力"
+              className="name-input form-control"
+            />
+          </div>
+          <div className="col">
+            <label htmlFor="name-input" className="form-label">
+              名：
+            </label>
+            <input
+              id="name-input"
+              type="text"
+              value={lastName}
+              onChange={(e) => setLastName(e.target.value)}
+              placeholder="名を入力"
+              className="name-input form-control"
+            />
+          </div>
         </div>
+        <button type="submit" className="add-button btn btn-primary">
+          登録
+        </button>
       </div>
     </>
   );
